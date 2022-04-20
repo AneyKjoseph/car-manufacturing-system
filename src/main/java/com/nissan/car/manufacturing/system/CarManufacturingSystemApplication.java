fa8bdffb034189a67c1a2e7b6310e012ddf0ef05
unknown <212402@C472LCOK.ustr.com>
2022-04-20 15:57:38 +0530

    initial commit

---
+++ src/main/java/com/nissan/car/manufacturing/system/CarManufacturingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nissan.car.manufacturing.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManufacturingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManufacturingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nissan/car/manufacturing/system/CarManufacturingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nissan.car.manufacturing.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManufacturingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
