c07d7425b2fb080dc5be052ade572a5facc3bf7c
AneyKjoseph <aneykjoseph@gmail.com>
2022-04-28 17:55:52 +0530

    CORS issue fix added

--- src/main/java/com/nissan/car/manufacturing/system/controller/PlantController.java
+++ src/main/java/com/nissan/car/manufacturing/system/controller/PlantController.java
@@ -3,6 +3,7 @@ package com.nissan.car.manufacturing.system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import java.util.Map;
  *
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class PlantController {
 	
 	@Autowired
--- src/main/java/com/nissan/car/manufacturing/system/controller/ZoneController.java
+++ src/main/java/com/nissan/car/manufacturing/system/controller/ZoneController.java
@@ -3,6 +3,7 @@ package com.nissan.car.manufacturing.system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import com.nissan.car.manufacturing.system.service.ZoneService;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ZoneController {
 
 	@Autowired
